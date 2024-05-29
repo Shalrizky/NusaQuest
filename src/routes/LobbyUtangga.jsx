e0efa47de15361982ab44058bdec0c169d4f6db0
Muhammad Shalrizky <shalrizky@gmail.com>
2024-05-29 23:33:26 +0700

    Menambahkan css untuk lobby Nuca dan Shadow pada font heading

--- src/routes/LobbyUtangga.jsx
+++ src/routes/LobbyUtangga.jsx
@@ -2,36 +2,56 @@ import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import roomUtanggaLogo from "../assets/common/utangga-lobby-logo.png";
 import "../style/routes/LobbyUtangga.css";
-import Header from '../components/common/Header';
+import Header from "../components/common/Header";
 
 function LobbyUtangga() {
   return (
     <Container fluid className="room-utangga-container">
-      <Header layout="profile" showLogos={false} showIcons={false} showBackIcon={true} />
+      <Header
+        layout="profile"
+        showLogos={false}
+        showIcons={false}
+        showBackIcon={true}
+      />
       <Row>
-        <Col md={12} className="text-white ps-5">
-          <h1 className="fw-bold display-4 display-md-3 display-sm-3">
+        <Col md={12} className="text-white ps-5 pt-2">
+          <h1
+            className="fw-bold display-5 display-md-3 display-sm-2"
+            id="heading-utangga"
+          >
             PERMAINAN ULAR TANGGA
           </h1>
         </Col>
       </Row>
       <Row>
-        <Col md={6} className="d-flex flex-column justify-content-start text-white p-5">
+        <Col
+          md={6}
+          className="d-flex flex-column justify-content-start text-white p-5"
+        >
           <h5 className="mb-4">Cara Bermain :</h5>
           <ol className="fs-6 text-start">
             <li className="mb-3">
-              Pilih salah satu dari lima room yang tersedia, mulai dari Room 1 hingga Room 5.
+              Pilih salah satu dari lima room yang tersedia, mulai dari Room 1
+              hingga Room 5.
             </li>
             <li className="mb-3">
-              Tunggu hingga pemain lain bergabung di room yang sama dengan minimal 2 pemain dan maksimal 4 pemain.
+              Tunggu hingga pemain lain bergabung di room yang sama dengan
+              minimal 2 pemain dan maksimal 4 pemain.
             </li>
             <li className="mb-3">
-              Permainan dimulai dengan setiap pemain secara bergantian melempar pertanyaan pilihan ganda yang telah disediakan terkait topik pada dek kartu, dengan masing-masing pemain mendapatkan lima kartu pertanyaan.
+              Permainan dimulai dengan setiap pemain secara bergantian melempar
+              dadu. Jika lemparan dadu menghasilkan angka 6, pemain akan
+              diberikan sebuah pertanyaan pilihan ganda terkait topik yang
+              dipilih. Jawab dengan benar untuk mendapatkan kesempatan melempar
+              dadu tambahan.
             </li>
           </ol>
           <Button className="btn-lobby-uTangga">Select Room</Button>
         </Col>
-        <Col md={6} className="d-flex justify-content-center align-items-md-center align-items-sm-center">
+        <Col
+          md={6}
+          className="d-flex justify-content-center align-items-md-center align-items-sm-center"
+        >
           <img
             src={roomUtanggaLogo}
             alt="Nusa Quest Logo"

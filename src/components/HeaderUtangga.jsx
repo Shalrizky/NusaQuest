fc57af7444fff8da0c583ad016c6bdcd22e88900
reykialmadani <reykialmadani09@gmail.com>
2024-09-10 19:02:47 +0700

    menambahkan background untuk halaman uler tangga

---
+++ src/components/HeaderUtangga.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Row, Col, Image } from "react-bootstrap"
+import useAuth from "../hooks/useAuth";
+import "../style/components/Header.css"
+
+function 
---
+++ src/routes/UlarTangga.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/routes/UlarTangga.css";  // Import file CSS untuk styling
+import bgUlarTangga from "../assets/common/bg-ular.png";  // Mengimpor background image
+
+function UlarTangga() {
+    return (
+        <Container fluid className="utangga-container">
+
+        </Container>
+    );
+}
+
+export default UlarTangga;

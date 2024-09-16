eec9a57f1feaf98a491c87a730b5302268ca5b9e
MariusJspvc <kamalabrar19@gmail.com>
2024-09-17 00:37:25 +0700

    commit di 17 september

--- src/routes/LobbyRoom.jsx
+++ src/routes/LobbyRoom.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import '../style/routes/LobbyRoom.css'; 
 import bgLobbyRoom from '../assets/common/background.png'; 
 import framePlayer from '../assets/common/FramePlayer.png'; 
+import EmptySlot from '../assets/common/EmptyPlayer.png';
 
 function LobbyRoom() {
     return (
         <Container fluid className="lobbyroom-container" style={{ backgroundImage: `url(${bgLobbyRoom})`, backgroundSize: 'cover' }}>
             <Row className="lobby-container">
                 <Col md={6} className="lobby-details">
-                    {/* Menampilkan gambar FramePlayer */}
-                    <Image src={framePlayer} alt="Frame Player" fluid /> {/* Gambar akan responsif */}
+                    <div className="player-slots">
+                        <Image src={framePlayer} alt="Frame Player" className="player-slot" />
+                        <Image src={EmptySlot} alt="Player Kosong" className="player-slot" />
+                        <Image src={EmptySlot} alt="Player Kosong" className="player-slot" />
+                        <Image src={EmptySlot} alt="Player Kosong" className="player-slot" />
+                        
+                    </div>
                 </Col>
             </Row>
         </Container>

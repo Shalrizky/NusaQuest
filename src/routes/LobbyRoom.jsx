3a81b65864cd323f28f46a8b68dfd1f364e32bb2
MariusJspvc <kamalabrar19@gmail.com>
2024-09-17 23:49:16 +0700

    menambahkan header

--- src/routes/LobbyRoom.jsx
+++ src/routes/LobbyRoom.jsx
@@ -4,10 +4,12 @@ import '../style/routes/LobbyRoom.css';
 import bgLobbyRoom from '../assets/common/background.png'; 
 import framePlayer from '../assets/common/FramePlayer.png'; 
 import EmptySlot from '../assets/common/EmptyPlayer.png';
+import ButtonStart from '../assets/common/btnStart.png';
 
 function LobbyRoom() {
     return (
         <Container fluid className="lobbyroom-container" style={{ backgroundImage: `url(${bgLobbyRoom})`, backgroundSize: 'cover' }}>
+            <h1>ROOM 1</h1>
             <Row className="lobby-container">
                 <Col md={6} className="lobby-details">
                     <div className="player-slots">
@@ -15,6 +17,11 @@ function LobbyRoom() {
                         <Image src={EmptySlot} alt="Player Kosong" className="player-slot" />
                         <Image src={EmptySlot} alt="Player Kosong" className="player-slot" />
                         <Image src={EmptySlot} alt="Player Kosong" className="player-slot" />
+                    </div>
+                    <div className="start-button-container" style={{ marginTop: '20px', textAlign: 'center' }}>
+                        <Button variant="primary" className="start-button" style={{ padding: 0, border: 'none', background: 'none' }}>
+                            <Image src={ButtonStart} alt="Button Start" />
+                        </Button>
                     </div>
                 </Col>
             </Row>

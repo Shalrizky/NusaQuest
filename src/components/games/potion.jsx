0200c85a900ea4537714f6ef5581e321f2c85874
Muhammad Shalrizky <shalrizky@gmail.com>
2024-10-04 18:32:51 +0700

    fix kiesalahan routihg asset ulartangga

---
+++ src/components/games/potion.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Image } from "react-bootstrap";
+import "../../style/components/games/potion.css";
+import potionIcon from "../../assets/games/Utangga/potion.png"; // Import icon potion
+
+function Potion() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleUsePotion = () => {
+    // Logika ketika potion digunakan
+    // alert("Potion digunakan!");
+    setShowModal(false); // Tutup modal setelah klik "Yes"
+    // Di sini kamu bisa melanjutkan permainan atau melakukan aksi lainnya
+  };
+
+  return (
+    <>
+      <div className="potion-icon mt-3" onClick={handleOpenModal}>
+        <Image src={potionIcon} alt="Potion Icon" width={100}  />
+      </div>
+
+      {/* Overlay hitam transparan dan konten modal langsung */}
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content-simple">
+            <p>Menggunakan Potion akan menyelamatkanmu dari pertanyaan ini.</p>
+            <p>Apakah Kamu Yakin Ingin Menggunakan Potion?</p>
+            <div className="modal-buttons">
+              <button className="modal-button yes" onClick={handleUsePotion}>
+                Ya
+              </button>
+              <button className="modal-button no" onClick={handleCloseModal}>
+                Tidak
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Potion;

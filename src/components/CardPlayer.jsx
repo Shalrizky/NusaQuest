125c653dea4709d5ec0e61c1213df7538bd62f0d
Muhammad Shalrizky <shalrizky@gmail.com>
2024-10-12 14:48:10 +0700

    Membenarkan Tampilan Card Active Player

--- src/components/CardPlayer.jsx
+++ src/components/CardPlayer.jsx
@@ -6,53 +6,55 @@ import vector from "../assets/common/Vector.png";
 import "../style/components/CardPlayer.css";
 
 const CardPlayer = () => {
-
   return (
     <Row className="card-player-container justify-content-center align-items-center mt-4 ">
-      <Col
-        className="card-box  d-flex  justify-content-center align-items-center gap-5 "
-        // ref={(el) => (cardRefs.current[0] = el)} // Assign ref ke array
-      >
-        <div className="card-wrapper">
+      <Col className="card-box d-flex justify-content-evenly align-items-center">
+        <div className="card-wrapper d-flex justify-content-center align-items-center">
           <Card className="card-player">
             <Card.Img variant="top" src={Image} className="img-card-player" />
             <Card.Body>
               <Card.Title className="title">KAMAL ABRAR</Card.Title>
-              <div className="card-image-container d-flex align-items-center">
-                <Card.Img src={badge1} alt="Player" className="badge-image" />
+              <div className="card-image-container d-flex align-items-center gap-2">
+                <Card.Img src={badge1} alt="badge" className="badge-image" />
                 <span className="badge-card-title">Master Kuliner</span>
               </div>
             </Card.Body>
           </Card>
         </div>
 
-        <div className="card-wrapper">
+        <div className="card-wrapper d-flex justify-content-center align-items-center">
           <Card className="card-player">
             <Card.Img variant="top" src={Image} className="img-card-player" />
             <Card.Body>
               <Card.Title className="title">KAMAL ABRAR</Card.Title>
-              <div className="card-image-container d-flex align-items-center">
-                <Card.Img src={badge1} alt="Player" className="badge-image" />
+              <div className="card-image-container d-flex align-items-center gap-2">
+                <Card.Img src={badge1} alt="badge" className="badge-image" />
                 <span className="badge-card-title">Master Kuliner</span>
               </div>
             </Card.Body>
           </Card>
         </div>
-        <div className="card-wrapper">
+
+        <div className="card-wrapper d-flex justify-content-center align-items-center">
           <Card className="card-player">
             <Card.Img variant="top" src={Image} className="img-card-player" />
             <Card.Body>
               <Card.Title className="title">KAMAL ABRAR</Card.Title>
-              <div className="card-image-container d-flex align-items-center">
-                <Card.Img src={badge1} alt="Player" className="badge-image" />
+              <div className="card-image-container d-flex align-items-center gap-2">
+                <Card.Img src={badge1} alt="badge" className="badge-image" width={50}/>
                 <span className="badge-card-title">Master Kuliner</span>
               </div>
             </Card.Body>
           </Card>
         </div>
+
         <div className="player-not-available">
           <Card className="player-not-available">
-            <Card.Img variant="top" src={vector} className="vector-player img-fluid" />
+            <Card.Img
+              variant="top"
+              src={vector}
+              className="vector-player img-fluid"
+            />
             <Card.Body>
               <Card.Text className="text-4">
                 <p>Menunggu Pemain Lain Untuk Masuk</p>

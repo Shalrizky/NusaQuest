74f1e275418abec9a44082270b88bb702ec2f247
Muhammad Shalrizky <shalrizky@gmail.com>
2024-10-24 00:39:25 +0700

    Menambahkan badge dan achievement di halaman profile langsung dari database

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -9,12 +9,18 @@ import {
   Modal,
   Form,
   Spinner,
+  OverlayTrigger,
+  Tooltip,
 } from "react-bootstrap";
 import {
   updateUserData,
   uploadPhoto,
   deletePreviousPhoto,
 } from "../services/userDataService";
+import {
+  checkIfAchievementExists,
+  getUserAchievements,
+} from "../services/achievementDataServices";
 import useAuth from "../hooks/useAuth";
 import useUserPhoto from "../hooks/useUserPhoto";
 import Header from "../components/Header";
@@ -33,12 +39,15 @@ function Profile() {
   const { user, logout } = useAuth();
   const [userData, setUserData] = useState(user);
   const [userPhoto, handlePhotoError] = useUserPhoto(userData);
+  const [userAchievements, setUserAchievements] = useState({});
+  const [loadingAchievements, setLoadingAchievements] = useState(true);
   const [show, setShow] = useState(false);
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
   const [toastVariant, setToastVariant] = useState("success");
   const [loading, setLoading] = useState(false);
 
+  // useState pada Modal Edit Profile
   const [newUsername, setNewUsername] = useState(user?.displayName || "");
   const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [previewPhoto, setPreviewPhoto] = useState(userData.photoURL);
@@ -51,6 +60,27 @@ function Profile() {
       hasChanges(userData.displayName, newUsername) || selectedPhoto !== null
     );
   }, [newUsername, selectedPhoto, userData.displayName]);
+
+  useEffect(() => {
+    const fetchAchievements = async () => {
+      try {
+        const exists = await checkIfAchievementExists(user.uid);
+        if (exists) {
+          const achievements = await getUserAchievements(user.uid);
+          setUserAchievements(achievements);
+        } else {
+          const achievements = await getUserAchievements(user.uid);
+          setUserAchievements(achievements);
+        }
+      } catch (error) {
+        console.error("Error fetching achievements:", error);
+      } finally {
+        setLoadingAchievements(false);
+      }
+    };
+
+    fetchAchievements();
+  }, [user.uid]);
 
   const handleClose = () => {
     setShow(false);
@@ -82,23 +112,23 @@ function Profile() {
 
   const handlePhotoChange = (e) => {
     const file = e.target.files[0];
-  
+
     if (!file) {
       setSelectedPhoto(null);
       setPreviewPhoto(userData.photoURL);
       return;
     }
-  
+
     // Preview the photo first
     const reader = new FileReader();
     reader.onloadend = () => {
       setPreviewPhoto(reader.result);
-  
+
       const validationError = validatePhoto(file);
-  
+
       if (validationError) {
         setPhotoError(validationError);
-        setSelectedPhoto(null); 
+        setSelectedPhoto(null);
       } else {
         setPhotoError(null);
         setSelectedPhoto(file);
@@ -106,63 +136,70 @@ function Profile() {
     };
     reader.readAsDataURL(file);
   };
-  
+
   const handleSaveChanges = async (event) => {
     event.preventDefault();
     setLoading(true);
-  
+
     const usernameValidation = validateUsername(newUsername);
     if (usernameValidation) {
       setUsernameError(usernameValidation);
       setLoading(false);
       return;
     }
-  
+
     if (photoError) {
       setLoading(false);
       return;
     }
-  
+
     try {
       const updatedUserData = {
         ...userData,
         displayName: newUsername, // Perbarui username dengan yang baru
       };
-  
+
       if (selectedPhoto) {
-        const isGooglePhoto = userData.photoURL?.startsWith("https://lh3.googleusercontent.com/");
+        const isGooglePhoto = userData.photoURL?.startsWith(
+          "https://lh3.googleusercontent.com/"
+        );
         if (userData.photoURL && !isGooglePhoto) {
           await deletePreviousPhoto(userData.photoURL);
         }
         const { downloadURL } = await uploadPhoto(selectedPhoto, userData.uid);
         updatedUserData.photoURL = downloadURL;
       }
-  
+
       // Simpan perubahan username dan photoURL ke Firebase Database
       await updateUserData(updatedUserData);
-  
+
       // Perbarui di local storage dan state aplikasi
       setLocalStorageItem("user", updatedUserData);
       setUserData(updatedUserData);
-  
+
       toggleShowToast("Profile updated successfully!", "success");
-  
+
       setTimeout(() => {
         window.location.reload();
       }, 500);
     } catch (error) {
-      toggleShowToast("Failed to update profile. Please try again later.", "danger");
+      toggleShowToast(
+        "Failed to update profile. Please try again later.",
+        "danger"
+      );
     } finally {
       setLoading(false);
       handleClose();
     }
   };
-  
-  
 
   return (
     <Container fluid id="profile-container">
-      <Header  showTextHeader="PROFILE" showBackIcon={true} showLogoIcon={false} />
+      <Header
+        showTextHeader="PROFILE"
+        showBackIcon={true}
+        showLogoIcon={false}
+      />
       <Row className="d-flex justify-content-center py-2 ">
         <Col md={3} className="d-flex align-items-center ">
           <Card id="profile-card">
@@ -194,14 +231,112 @@ function Profile() {
           </Card>
         </Col>
         <Col md={9} className="d-flex align-items-center">
-          <Card id="profile-card-Achievement">
+          <Card id="profile-card-Achievement" className="w-100">
             <Card.Body id="profile-card-Achievement-content">
-              <div className="text-white">
-                <h4 className="fw-bold">Badge</h4>
+              {/* Your Badges Section */}
+              <div className="text-white mb-4">
+                <h4 className="fw-bold">Your Badges</h4>
               </div>
-              <div className="text-white">
-                <h4 className="fw-bold">Achievement</h4>
+
+              {loadingAchievements ? (
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              ) : Object.keys(userAchievements).length === 0 ? (
+                // Display "You don’t have any badges" if no badges exist
+                <div className="text-white text-center">
+                  <h5>You don’t have any badges</h5>
+                </div>
+              ) : (
+                <div className="badge-container d-flex flex-wrap gap-4 align-items-center mb-4">
+                  {Object.keys(userAchievements).map((game) =>
+                    Object.keys(userAchievements[game]).map((topic) => {
+                      const achievement = userAchievements[game][topic];
+                      return (
+                        <div
+                          key={topic}
+                          className="badge-item d-flex flex-column align-items-center"
+                        >
+                          {/* Badge (always displayed with tooltip) */}
+                          <OverlayTrigger
+                            placement="top"
+                            overlay={
+                              <Tooltip
+                                id={`tooltip-badge-${topic}`}
+                                className="custom-tooltip"
+                              >
+                                {achievement.badge.badgeName}
+                              </Tooltip>
+                            }
+                            container={document.body}
+                          >
+                            <div className="badge-item">
+                              <Image
+                                src={achievement.badge.iconURL}
+                                alt={achievement.badge.badgeName}
+                                className="badge-image-profile"
+                              />
+                            </div>
+                          </OverlayTrigger>
+                        </div>
+                      );
+                    })
+                  )}
+                </div>
+              )}
+
+              {/* Your Achievements Section */}
+              <div className="text-white mb-4 mt-5">
+                <h4 className="fw-bold">Your Achievements</h4>
               </div>
+
+              {loadingAchievements ? (
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              ) : Object.keys(userAchievements).length === 0 ||
+                Object.keys(userAchievements).every((game) =>
+                  Object.keys(userAchievements[game]).every(
+                    (topic) => userAchievements[game][topic].totalWins < 5
+                  )
+                ) ? (
+                // Display "You don’t have any achievements" if no achievements with wins exist
+                <div className="text-white text-center">
+                  <h5>You don’t have any achievements</h5>
+                </div>
+              ) : (
+                <div className="achievement-container d-flex flex-wrap gap-4 align-items-center mb-4">
+                  {Object.keys(userAchievements).map((game) =>
+                    Object.keys(userAchievements[game]).map((topic) => {
+                      const achievement = userAchievements[game][topic];
+
+                      // Show achievements only if totalWins >= 5
+                      if (achievement.totalWins >= 5) {
+                        return (
+                          <div
+                            key={topic}
+                            className="achievement-item d-flex flex-column align-items-center"
+                          >
+                            <div className="trophy-item mt-3">
+                              <Image
+                                src={achievement.achievement_trophy}
+                                alt={achievement.achievement_name}
+                                className="trophy-image-profile"
+                              />
+                              {/* Display the title of the achievement */}
+                              <div className="text-white mt-2">
+                                {achievement.achievement_name}
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      }
+
+                      return null; // If totalWins < 5, do not return anything
+                    })
+                  )}
+                </div>
+              )}
             </Card.Body>
           </Card>
         </Col>
--- src/services/achievementDataServices.jsx
+++ src/services/achievementDataServices.jsx
@@ -1,34 +1,41 @@
 import { database, ref, set, get } from "../firebaseConfig";
 
-// Fungsi membuat data achievement untuk user 
+// Fungsi membuat data achievement untuk user
 export const initializeUserAchievement = async (uid) => {
   if (!uid) {
     throw new Error("User ID is missing. Cannot initialize achievements.");
   }
 
-  const games = ["game1", "game2"];
+  // Define the game names and topics
+  const games = [
+    { key: "game1", prefix: "Master Ular Tangga" },
+    { key: "game2", prefix: "Master Nuca" },
+  ];
+
   const topics = [
-    { key: "daerah_jawa_barat", name: "Master Daerah Jawa Barat" },
-    { key: "kuliner_jawa_barat", name: "Master Kuliner Jawa Barat" },
-    { key: "pariwisata_bahari", name: "Master Pariwisata Bahari" },
-    { key: "pariwisata_darat", name: "Master Pariwisata Darat" },
-    { key: "permainan_daerah", name: "Master Permainan Tradisional" },
+    { key: "daerah_jawa_barat", name: "Daerah Jawa Barat" },
+    { key: "kuliner_jawa_barat", name: "Kuliner Jawa Barat" },
+    { key: "pariwisata_bahari", name: "Pariwisata Bahari" },
+    { key: "pariwisata_darat", name: "Pariwisata Darat" },
+    { key: "permainan_daerah", name: "Permainan Tradisional" },
   ];
 
   // Membuat achievement data langsung dalam fungsi ini
-  const achievements = {}
+  const achievements = {};
+
   games.forEach((game) => {
-    achievements[game] = {};
+    achievements[game.key] = {}; // Store data under each game key
+
     topics.forEach((topic) => {
-      achievements[game][topic.key] = {
+      // Add the game-specific prefix to the achievement name
+      achievements[game.key][topic.key] = {
         totalWins: 0,
-        achievement_name: topic.name,
+        achievement_name: `${game.prefix} ${topic.name}`, // Add prefix here
+        achievement_trophy: "/assets/game/trophy.png",
         badge: {
-          badgeName: "No Badge",
-          iconURL: "/assets/game/no-badge.png",
-          level: "None",
+          badgeName: getBadgeName(0, game.prefix, topic.name), // Dynamic badge name based on wins, game, and topic
+          iconURL: getBadgeIconURL(0), // Dynamic badge icon based on wins
         },
-        achievementAwarded: false,
       };
     });
   });
@@ -36,26 +43,65 @@ export const initializeUserAchievement = async (uid) => {
   await saveNewAchievementsData(uid, achievements);
 };
 
+// Fungsi untuk menentukan nama badge berdasarkan total kemenangan
+const getBadgeName = (totalWins, gamePrefix, topicName) => {
+   const gameName = gamePrefix.replace("Master ", ""); // Menghilangkan "Master"
+   
+   if (totalWins >= 5) {
+     return `Gold Badge - ${totalWins} Wins in ${gameName} ${topicName}`;
+   } else if (totalWins >= 3) {
+     return `Silver Badge - ${totalWins} Wins in ${gameName} ${topicName}`;
+   } else if (totalWins >= 1) {
+     return `Bronze Badge - ${totalWins} Wins in ${gameName} ${topicName}`;
+   } else {
+     // Menampilkan game dan topik tanpa kata "Master" untuk No Badge
+     return `No Badge in ${topicName} (${gameName})`;
+   }
+ };
+ 
+
+// Fungsi untuk menentukan icon badge berdasarkan total kemenangan
+const getBadgeIconURL = (totalWins) => {
+  if (totalWins >= 5) {
+    return "/assets/game/gold-badge.png";
+  } else if (totalWins >= 3) {
+    return "/assets/game/silver-badge.png";
+  } else if (totalWins >= 1) {
+    return "/assets/game/bronze-badge.png";
+  } else {
+    return "/assets/game/no-badge.png";
+  }
+};
 
 // Fungsi untuk menyimpan atau menginisialisasi data achievement ke Firebase
 export const saveNewAchievementsData = async (uid, achievementData) => {
-   try {
-     await set(ref(database, `achievements/${uid}`), achievementData);
-   } catch (error) {
-     console.error("Error initializing achievement data:", error);
-     throw new Error("Could not initialize achievement data");
-   }
- };
- 
- // Fungsi untuk mengecek apakah user sudah memiliki data achievement
- export const checkIfAchievementExists = async (uid) => {
-   try {
-     const achievementsRef = ref(database, "achievements/" + uid);
-     const snapshot = await get(achievementsRef);
-     return snapshot.exists();
-   } catch (error) {
-     console.error("Error checking achievement data:", error);
-     throw new Error("Could not check achievement data");
-   }
- };
- 
+  try {
+    await set(ref(database, `achievements/${uid}`), achievementData);
+  } catch (error) {
+    console.error("Error initializing achievement data:", error);
+    throw new Error("Could not initialize achievement data");
+  }
+};
+
+// Fungsi untuk mengecek apakah user sudah memiliki data achievement
+export const checkIfAchievementExists = async (uid) => {
+  try {
+    const achievementsRef = ref(database, "achievements/" + uid);
+    const snapshot = await get(achievementsRef);
+    return snapshot.exists();
+  } catch (error) {
+    console.error("Error checking achievement data:", error);
+    throw new Error("Could not check achievement data");
+  }
+};
+
+export const getUserAchievements = async (uid) => {
+  try {
+    const achievementsRef = ref(database, "achievements/" + uid);
+    const snapshot = await get(achievementsRef);
+    return snapshot.val(); // Return the achievement data
+  } catch (error) {
+    console.error("Error fetching user achievements:", error);
+    throw new Error("Could not fetch user achievements");
+  }
+};

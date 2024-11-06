dad1b1cf283acce48973036154222854eaca06cc
Muhammad Shalrizky <shalrizky@gmail.com>
2024-11-06 16:17:58 +0700

    Menambahkan component loseOverlay dan aset

---
+++ src/components/games/LoseOverlay.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import defeatImage from "../../assets/games/defeat.png";
+import "../../style/components/games/loseOverlay.css";
+
+function LoseOverlay({ loser, onClose }) {
+  // Tutup overlay secara otomatis setelah 10 detik
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  return (
+    <div className="lose-overlay" onClick={onClose}>
+      <div className="lose-content" onClick={(e) => e.stopPropagation()}>
+        <img src={defeatImage} alt="Defeat Logo" className="lose-logo" />
+        <h2>{loser} Defeat!</h2>
+        <p>Semangat, semoga beruntug di lain waktu</p>
+        <p>
+          Sentuh dimana saja untuk keluar <br /> /{" "}
+          <span className="auto-redirect-info">
+            Anda akan diarahkan keluar dalam waktu 10 detik...
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default LoseOverlay;
